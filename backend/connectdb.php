515c6ef2473f88a014b3c5345f98affd6e09fce2
WesleyJoshuaPerez <122728678+WesleyJoshuaPerez@users.noreply.github.com>
2025-03-02 07:43:57 +0800

    connectdb add

---
+++ backend/connectdb.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "lynx";
+
+// Create a new mysqli connection
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo"Connection success";
+?>
