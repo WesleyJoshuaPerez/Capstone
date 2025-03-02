153a9c3365ecad3958b616d5e4e5f480f6858da5
kate <katerezada0120@gmail.com>
2025-03-02 10:34:17 +0800

    registraction_acc table

--- database/registration_acc.sql
+++ database/registration_acc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 01, 2025 at 04:14 PM
+-- Generation Time: Mar 02, 2025 at 03:33 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -44,11 +44,19 @@ CREATE TABLE `registration_acc` (
   `barangay` varchar(100) DEFAULT NULL,
   `proof_of_residency` varchar(255) DEFAULT NULL,
   `installation_date` date DEFAULT NULL,
-  `consent_terms_conditions` tinyint(1) DEFAULT 0,
-  `consent_data_processing` tinyint(1) DEFAULT 0,
-  `consent_id_photo_collection` tinyint(1) DEFAULT 0,
-  `registration_date` timestamp NOT NULL DEFAULT current_timestamp()
+  `registration_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `terms_agreed` varchar(10) NOT NULL DEFAULT 'Unchecked',
+  `data_processing_consent` varchar(10) NOT NULL DEFAULT 'Unchecked',
+  `id_photo_consent` varchar(10) NOT NULL DEFAULT 'Unchecked'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `registration_acc`
+--
+
+INSERT INTO `registration_acc` (`id`, `subscription_plan`, `first_name`, `last_name`, `contact_number`, `email_address`, `birth_date`, `id_type`, `id_number`, `id_photo`, `home_ownership_type`, `province`, `municipality`, `barangay`, `proof_of_residency`, `installation_date`, `registration_date`, `terms_agreed`, `data_processing_consent`, `id_photo_consent`) VALUES
+(1, 'silver', 'kate', 'rezada', '09054627399', 'katerezada0120@gmail.com', '2007-03-01', 'drivers-license', '', 'uploads/yNdMmf8.jpg', 'Rented', 'bataan', 'limay', 'alangan', 'uploads/yNdMmf8.jpg', '2025-03-19', '2025-03-02 02:22:58', 'Checked', 'Checked', 'Checked'),
+(2, 'silver', 'kate', 'rezada', '09054627399', 'katerezada0120@gmail.com', '2007-03-02', 'drivers-license', '', 'uploads/yNdMmf8.jpg', 'Rented', 'bataan', 'limay', 'kitang', 'uploads/yNdMmf8.jpg', '2025-03-14', '2025-03-02 02:27:07', 'Checked', 'Checked', 'Checked');
 
 --
 -- Indexes for dumped tables
@@ -68,7 +76,7 @@ ALTER TABLE `registration_acc`
 -- AUTO_INCREMENT for table `registration_acc`
 --
 ALTER TABLE `registration_acc`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
