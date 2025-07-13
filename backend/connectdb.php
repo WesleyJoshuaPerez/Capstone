95e4e86d92ef66296eb600feb45839fc27d0d393
WesleyJoshuaPerez <122728678+WesleyJoshuaPerez@users.noreply.github.com>
2025-07-13 21:31:29 +0800

    use to enable database connection both live and local

--- backend/connectdb.php
+++ backend/connectdb.php
@@ -1,8 +1,18 @@
 <?php
-$host = "localhost";
-$user = "u242690062_lynx";
-$pass = "Capstonelynx2025";
-$db = "u242690062_lynx";
+// Check if the server is localhost this if else statement use so the code can work live or localhost
+if ($_SERVER['HTTP_HOST'] === 'localhost') {
+    // Localhost database credentials
+    $host = "localhost";
+    $user = "root";  
+    $pass = "";      
+    $db   = "lynx"; 
+} else {
+    // Hostinger database credentials
+    $host = "localhost";  
+    $user = "u242690062_lynx";
+    $pass = "Capstonelynx2025";
+    $db   = "u242690062_lynx";
+}
 
 // Create a new mysqli connection
 $conn = new mysqli($host, $user, $pass, $db);
@@ -10,6 +20,5 @@ $conn = new mysqli($host, $user, $pass, $db);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
- 
+}
 ?>
