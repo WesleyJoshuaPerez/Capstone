d012d8a52cfbbca5f6b84543306b290dec315153
kate <katerezada0120@gmail.com>
2025-07-31 15:28:38 +0800

    swal before logging in for overdue bill

--- scripts/login.js
+++ scripts/login.js
@@ -86,7 +86,7 @@ document.addEventListener("DOMContentLoaded", function () {
               Swal.fire({
                 icon: "warning",
                 title: "Billing Reminder",
-                text: "You have an overdue bill. Please settle it as soon as possible.",
+                text: "You have an overdue bill. Please settle it within (5) days after your supposed due date or contact our office to avoid termination of your account.",
                 confirmButtonText: "Got it",
               }).then(() => {
                 Swal.fire({
